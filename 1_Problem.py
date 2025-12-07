b770b03b474eea164ee5b3949ddaa44f7c18d9a8
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-07 15:47:44 +0530

    Take a number and print whether it’s positive, negative, or zero

---
+++ 1_Problem.py
@@ -0,0 +1,9 @@
+# Take a number and print whether it’s positive, negative, or zero. 
+num = int(input("Enter a number : "))
+
+if num == 0:
+    print("Zero")
+elif num > 0:
+    print("Positive")
+else: 
+    print("Negative")
